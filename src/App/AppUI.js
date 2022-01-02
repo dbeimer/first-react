71d14b422a6f346bc08f253acff30c52687777ad
Beimer Campos <beimercampos.123@gmail.com>
2022-01-02 15:21:04 -0500

    working with states

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,41 +1,40 @@
 import React from "react";
-import './style.css'
+import "./style.css";
 
-import {TodoCounter} from "../TodoCounter";
-import {TodoSearch} from "../TodoSearch";
-import {TodoList} from "../TodoList";
-import {TodoItem} from "../TodoItem";
-import {CreateTodoBotton} from "../CreateTodoBotton";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { CreateTodoBotton } from "../CreateTodoBotton";
 
 function AppUI({
+  loading,
+  error,
   totalTodos,
   completedTodos,
   searchValue,
   setSearchValue,
   searchedTodos,
   completeTodos,
-  deleteTodos}) {
-
+  deleteTodos,
+}) {
   return (
     <React.Fragment>
-      <TodoCounter
-        totalTodos={totalTodos}
-        completedTodos={completedTodos}
-      />
+      <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
 
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {searchedTodos.map(todo => (
+        {error && <p>Algo salio mal</p>}
+        {loading && <p>Estamos cargando no desesperes...</p>}
+        {!loading && !searchedTodos.length && <p>!Crea tu primer todo!</p>}
+
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
             onComplete={completeTodos}
             onDelete={deleteTodos}
-
           />
         ))}
       </TodoList>
@@ -44,6 +43,4 @@ function AppUI({
   );
 }
 
-export {AppUI};
-
-
+export { AppUI };
--- src/App/index.js
+++ src/App/index.js
@@ -1,71 +1,95 @@
 import React from "react";
-import {AppUI} from "./AppUI";
+import { AppUI } from "./AppUI";
 
 function useLocalStorage(itemName, initialValue) {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
 
-  const localStorageItem = localStorage.getItem(itemName);
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
 
-  let parsedItem;
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = []
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
+        let parsedItem;
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = [];
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
 
-  const [item, setItem] = React.useState(parsedItem)
+        setItem(parsedItem);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        /* handle error */
+      }
+    }, 1000);
+  });
 
   const saveItem = (newItem) => {
-    const stringifiedItem = JSON.stringify(newItem);
-    localStorage.setItem(itemName, stringifiedItem);
-    setItem(newItem);
-  }
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
+  };
 
-  return [
+  return {
     item,
-    saveItem
-  ];
+    saveItem,
+    loading,
+    error,
+  };
 }
 
 function App() {
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_V1", []);
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
-
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
 
   let searchedTodos = todos;
 
   let totalTodos = searchedTodos.length;
-  let completedTodos = searchedTodos.filter(todo => !!todo.completed).length;
+  let completedTodos = searchedTodos.filter((todo) => !!todo.completed).length;
 
   if (searchValue.length > 0) {
-    searchedTodos = todos.filter(todo => {
+    searchedTodos = todos.filter((todo) => {
       const searchText = searchValue.toLowerCase();
-      const todoText = todo.text.toLowerCase()
-      return todoText.includes(searchText)
-    })
+      const todoText = todo.text.toLowerCase();
+      return todoText.includes(searchText);
+    });
 
     totalTodos = searchedTodos.length;
-    completedTodos = searchedTodos.filter(todo => !!todo.completed).length;
-
+    completedTodos = searchedTodos.filter((todo) => !!todo.completed).length;
   }
 
-
   const completeTodos = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
     saveTodos(newTodos);
-  }
+  };
 
   const deleteTodos = (text) => {
-    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
     saveTodos(newTodos);
-  }
+  };
+
   return (
     <AppUI
+      error={error}
+      loading={loading}
       totalTodos={totalTodos}
       completedTodos={completedTodos}
       searchValue={searchValue}
